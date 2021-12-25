3bba3237fa398f34b7ffa436eb0f1594c28bf832
Zach Ross <zrross11@gmail.com>
2021-12-24 22:09:47 -0500

    potential whitenoise fix

--- website/settings.py
+++ website/settings.py
@@ -124,6 +124,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Default primary key field type
